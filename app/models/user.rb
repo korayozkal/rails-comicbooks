9255e5fb670f07be9339863a16df7da6918f4e1d
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-09-09 18:45:24 -0700

    User associations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+has_many :collections
+has_many :comicbooks, through: :collections 
+
+#has_secure_password => activate when setting up user
+#Note to myself => validations 
+
 end
