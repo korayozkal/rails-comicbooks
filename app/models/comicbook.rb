f2616acc9ac6ea4f254a82eec78015160573f2a3
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-09-09 18:49:35 -0700

    Collection Associations created

--- app/models/comicbook.rb
+++ app/models/comicbook.rb
@@ -1,2 +1,4 @@
 class Comicbook < ApplicationRecord
+    
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-has_many :collections
-has_many :comicbooks, through: :collections 
+  has_many :collections
+  has_many :comicbooks, through: :collections 
 
 #has_secure_password => activate when setting up user
 #Note to myself => validations 
