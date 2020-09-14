47eb92c6112d0e274491569d1b31cb199023fb1a
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-09-14 15:38:52 -0700

    collection controller actions added

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,23 @@
 class CollectionsController < ApplicationController
+    def new
+        @collection = Collection.new
+        @comicbook_id = params[:comicbook]
+        @comicbooks = Comicbook.all
+      end
+    
+      def create
+        @collection = current_user.reviews.build(collection_params)
+        if @collection.save
+          redirect_to @collection.book, notice: "Review successfully created."
+        else
+          render :new
+        end
+      end
+    
+      private
+    
+      def collection_params
+        params.require(:collection).permit(:rating, :comicbook_id, :review)
+      end
+    
 end
