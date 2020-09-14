018cf20827444a56368b68743101b5b332793550
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-09-14 15:10:36 -0700

    comicbooks controller actions added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+        def current_user
+          @user ||= User.last
+        end
+      end
+
 end
+
+#Double check
--- app/controllers/comicbooks_controller.rb
+++ app/controllers/comicbooks_controller.rb
@@ -1,7 +1,55 @@
 class ComicbooksController < ApplicationController
+before_action :find_comicbook, only: [:show, :edit, :update, :destroy]
     
     def index
-        @comicbook= Comicbook.all 
+        @comicbooks= Comicbook.all 
+    end 
+ 
+    def show
+    end
+
+    def new
+        @comicbook = Comicbook.new
+    end
+
+    def edit
+    end
+
+    def create
+    #@comicbook = Comicbook.new(comicbook_params)
+    #comicbook.user = current_user  
+    @comicbook = current_user.comicbooks.build(comicbook_params)
+        if @comicbook.save
+            redirect_to @comicbook, notice: 'Comicbook was successfully created.'
+        else
+            render :new
+        end
+    end
+
+    def update
+
+        if @comicbook.update(book_params)
+           redirect_to @comicbook, notice: 'Book was successfully updated.'
+        else 
+           render :edit
+        end 
     end 
 
-end
+    def destroy
+        @comicbook.destroy
+        render :edit
+    
+    private
+
+    def find_comicbook
+        @comicbook = Comicbook.find(params[:id])
+    end 
+
+    def book_params
+        params.require(:book).permit(:title, :number, :writer, :artist, :publisher)
+    end
+
+
+    
+
+
