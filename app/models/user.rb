8c380a4d5a1bb8ec9eb1c491dd6f6b3f0d108b9e
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-09-16 13:06:33 -0700

    validations added to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   has_many :collections
   has_many :comicbooks, through: :collections 
-  
-#validates_presence_of :username, uniquness: true 
-#has_secure_password => activate when setting up user
-#Note to myself => validations 
+  has_secure_password 
+
+  validates_presence_of :username, uniquness: true 
 
 end
