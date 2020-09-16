fc261fb6c07073792bc18be35758e843145ebd25
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-09-16 13:25:11 -0700

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  resources :collections
-  resources :comicbooks
-  resources :users, except: [:new]
+ 
+root to: 'static#index' #update to welcome
+  
+get '/signup', to: 'users#new'
+get '/login', to: 'sessions#new',  as: 'login' 
+post '/login', to: 'sessions#create'  
+delete '/logout', to: 'sessions#destroy'
+
+resources :users, except: [:new]
+resources :comicbooks do 
+  resources :reviews
+end 
 
   #get '/signup' to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -10,3 +19,4 @@ Rails.application.routes.draw do
   #post '/login', to: 'sessions#create'
   #delete '/logout', to: 'sessions#destroy'
 end 
+
